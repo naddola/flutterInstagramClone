56f3521b1711d34506f23087b7792a6bd54abee2
hcjeon <hcjeon@globalorder.co.kr>
2022-11-20 20:18:27 +0900

    feat : create search page

---
+++ lib/search_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Icon(Icons.create),
+        backgroundColor: Colors.blue,
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 1.0,
+            mainAxisSpacing: 2.0,
+            crossAxisSpacing: 2.0),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return _buildListItem(context, index);
+        });
+  }
+
+  Widget _buildListItem(BuildContext context, int index) {
+    return Image.network('https://cdn.spotvnews.co.kr/news/photo/202209/549843_768741_590.jpg',
+    fit: BoxFit.cover,);
+  }
+}
--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instaclon/account_page.dart';
 import 'package:instaclon/home_page.dart';
+import 'package:instaclon/search_page.dart';
 
 class TabPage extends StatefulWidget {
   const TabPage({Key? key}) : super(key: key);
@@ -13,7 +14,7 @@ class _TabPageState extends State<TabPage> {
   int _currentTabIndex = 0;
   List _pages = [
     HomePage(),
-    Text('page2'),
+    SearchPage(),
     AccountPage(),
   ];
 
