e98fbf98789992c8b3c5af028ccf3bf356f65870
hcjeon <hcjeon@woomsoft.com>
2022-11-21 12:16:28 +0900

    feat : image picker

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePage extends StatefulWidget {
   const CreatePage({Key? key}) : super(key: key);
@@ -10,13 +13,15 @@ class CreatePage extends StatefulWidget {
 class _CreatePageState extends State<CreatePage> {
   final textEditingController = TextEditingController();
 
+  File? _image;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: _getImage,
         child: Icon(Icons.add_a_photo),
       ),
     );
@@ -33,14 +38,16 @@ class _CreatePageState extends State<CreatePage> {
   }
 
   Widget _buildBody() {
-    return Column(
-      children: [
-        Text('noimage'),
-        TextField(
-          decoration: InputDecoration(hintText: '내용을 입력해주세요', contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
-          controller: textEditingController,
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          _image == null ? Text('noimage') : Image.file(_image!),
+          TextField(
+            decoration: InputDecoration(hintText: '내용을 입력해주세요', contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
+            controller: textEditingController,
+          )
+        ],
+      ),
     );
   }
 
@@ -48,5 +55,13 @@ class _CreatePageState extends State<CreatePage> {
   void dispose() {
     textEditingController.dispose();
     super.dispose();
+  }
+
+  Future _getImage() async {
+    ImagePicker _picker = ImagePicker();
+    XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(image!.path);
+    });
   }
 }
